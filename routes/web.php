16369852d27d1910f182890c2120347d0fb83acc
Luis Anillo <luis1104anillo@gmail.com>
2023-09-15 10:05:30 -0500

    Routes

--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,28 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PetController;
+//use App\Http\Controllers\PruebaController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// ----------------------------------------------------------
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Home
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('pets', PetController::class)->except(['show']);
+    //Route::put('/edit/{pet}', [PruebaController::class, 'update'])->name('edit.posst');
 });
 
 require __DIR__.'/auth.php';
